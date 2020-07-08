5812a195f0d346d6c541d84bcac6932e22c167a5
Guido De Gobbis <guido@degobbis.de>
2020-07-08 19:13:59 +0200

    Hook bei activierung überarbeitet
    Gewünschte Einstellungen werden automatisch bei Aktivierung des Plugins gespeichert.
    
    Bei Multisites geschieht das für jede Site und das Plugin wird automatisch netzwerkweit aktiviert.

--- selfhost-google-fonts/bootstrap.php
+++ selfhost-google-fonts/bootstrap.php
@@ -1,33 +1,33 @@
 <?php
 /**
  * Plugin bootstrap
- * 
+ *
  * @since   1.0.1
  */
 
 defined('WPINC') || exit;
 
 if (version_compare(phpversion(), '5.4', '<')) {
-		
+
 	/**
 	 * Display an admin error notice when PHP is older the version 5.4
 	 * Hook it to the 'admin_notices' action.
 	 */
 	function sgf_old_php_admin_error_notice() {
-		
+
 		$message = sprintf(esc_html__(
-			'The %2$sSelf-Hosted Google Fonts%3$s plugin requires %2$sPHP 5.4+%3$s to run properly. Please contact your web hosting company and ask them to update the PHP version of your site.%4$s Your current version of PHP has reached end-of-life is %2$shighly insecure: %1$s%3$s', 'sphere-sgf'), 
-			phpversion(), 
-			'<strong>', 
-			'</strong>', 
+			'The %2$sSelf-Hosted Google Fonts%3$s plugin requires %2$sPHP 5.4+%3$s to run properly. Please contact your web hosting company and ask them to update the PHP version of your site.%4$s Your current version of PHP has reached end-of-life is %2$shighly insecure: %1$s%3$s', 'sphere-sgf'),
+			phpversion(),
+			'<strong>',
+			'</strong>',
 			'<br>'
 		);
 
 		printf('<div class="notice notice-error"><p>%1$s</p></div>', wp_kses_post($message));
 	}
-	
+
 	add_action('admin_notices', 'sgf_old_php_admin_error_notice');
-	
+
 	// bail
 	return;
 }
@@ -53,15 +53,3 @@ $plugin->plugin_file = __FILE__;
 
 // Init on plugins loaded
 add_action('plugins_loaded', array($plugin, 'init'));
-
-/**
- * Register activation and deactivation hooks
- */
-
-register_activation_hook(__FILE__, function() {
-	// Noop
-});
-
-register_deactivation_hook(__FILE__, function() {
-	// Noop
-});
--- selfhost-google-fonts/selfhost-google-fonts.php
+++ selfhost-google-fonts/selfhost-google-fonts.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Self-Hosted Google Fonts
- * 
+ *
  * @package           Sphere\SGF
  *
  * Plugin Name:       Self-Hosted Google Fonts
@@ -18,8 +18,58 @@
 
 defined('WPINC') || exit;
 
-// Not so easy. Setting this to true on free version will give FATAL errors as some 
+// Not so easy. Setting this to true on free version will give FATAL errors as some
 // files are only in pro version. No cheating.
 define('SGF_IS_PRO', false);
+
+/**
+ * Register activation and deactivation hooks
+ */
+global $sgf_is_done;
+$sgf_is_done = array();
+
+register_activation_hook(__FILE__, function () {
+	global $sgf_is_done;
+
+	$sgf_options = unserialize('a:5:{s:7:"enabled";s:1:"1";s:16:"process_enqueues";s:2:"on";s:17:"process_css_files";s:2:"on";s:18:"process_css_inline";s:2:"on";s:17:"protocol_relative";s:2:"on";}');
+
+	if (is_multisite())
+	{
+		if (empty($sgf_is_done))
+		{
+			$sgf_is_done = true;
+			activate_plugins(plugin_basename(__FILE__), '', true);
+		}
+
+		$blogIds = get_sites(array('fields' => 'ids'));
+
+		foreach ($blogIds as $blogId)
+		{
+			switch_to_blog($blogId);
+
+			if (!get_option('sgf_options'))
+			{
+				update_option('sgf_options', $sgf_options);
+			}
+
+			restore_current_blog();
+		}
+
+		return;
+	}
+
+	if (get_option('sgf_options'))
+	{
+		return;
+	}
+
+	update_option('sgf_options', $sgf_options);
+
+	return;
+});
+
+register_deactivation_hook(__FILE__, function () {
+	return;
+});
 
 require_once plugin_dir_path(__FILE__) . 'bootstrap.php';
